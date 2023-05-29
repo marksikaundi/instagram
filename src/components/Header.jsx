c6c57314e9cf719f25d5fa15db7644da46310327
marksikaundi <emmanuelmark117@gmail.com>
2023-05-29 23:10:57 +0200

    header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,24 @@ export default function Header() {
   return (
     <div>
       {/* left */}
-      <div className="">
-        <div className="">
+      <div className="flex items-center justify-between">
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
           <Image
             src="https://th.bing.com/th/id/R.825e4d7b40faa8f9c51da0c73d6254d8?rik=RVfV80ATb%2boUhg&pid=ImgRaw&r=0"
-            width={40}
-            height={40}
+            layout="fill"
             alt="Logo"
+            className="object-contain"
           />
         </div>
+        <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+          <Image
+            src="https://maltaineu.gov.mt/en/PublishingImages/Pages/Welcome/INSTA%20LOGO.jpeg"
+            layout="fill"
+            alt="Logo"
+            className="object-contain"
+          />
+        </div>
+        <h1>Right side</h1>
       </div>
 
       {/* middle */}
