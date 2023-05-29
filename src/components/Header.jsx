6da393cf4e19b796835b8eeb97ea635a34d91093
marksikaundi <emmanuelmark117@gmail.com>
2023-05-29 22:41:20 +0200

    added img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Header() {
   return (
     <div>
-      Header
+      {/* left */}
+      <div className="">
+        <div className="">
+          <Image
+          src="https://th.bing.com/th/id/R.825e4d7b40faa8f9c51da0c73d6254d8?rik=RVfV80ATb%2boUhg&pid=ImgRaw&r=0"
+           />
+        </div>
+      </div>
+
+      {/* middle */}
+
+      {/* right */}
     </div>
-  )
+  );
 }
