4efe215822910c07bad46fcb33e71e79f71179e8
marksikaundi <emmanuelmark117@gmail.com>
2023-05-29 23:13:20 +0200

    left side bar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div>
       {/* left */}
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between max-w-6xl">
         <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
           <Image
             src="https://th.bing.com/th/id/R.825e4d7b40faa8f9c51da0c73d6254d8?rik=RVfV80ATb%2boUhg&pid=ImgRaw&r=0"
